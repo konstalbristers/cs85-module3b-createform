08ed1111bdba0bfb752ac4c112433df99358057f
konstalbristers <kcparis33@gmail.com>
2025-07-21 18:42:21 -0700

    added if statement that checks if you didnt enter a name to tell you to enter a name, otherwise if the strlen of the name that you entered is less than or equal to 2 it tells you to put a name longer than 2

--- Form.php
+++ Form.php
@@ -8,22 +8,30 @@
 <body>
     <form action="Form.php" method="POST">
         <label for="fullname">Full name: </label>
-        <input type="text" id="fullname" name="full_name" placeholder="John Doe" required><br><br>
+        <input type="text" id="fullname" name="full_name" placeholder="John Doe"><br><br>
         <label for="email">E-mail Address: </label>
-        <input type="text" id="email" name="email_address" placeholder="example@email.com" required><br><br>
+        <input type="text" id="email" name="email_address" placeholder="example@email.com"><br><br>
         <label for="topic">Topic: </label>
-        <input type="text" id="topic" name="topic" required><br><br>
+        <input type="text" id="topic" name="topic"><br><br>
         <label for="message">Message: </label>
-        <textarea id="message" name="message" placeholder="Enter your message here" required></textarea><br><br>
+        <textarea id="message" name="message" placeholder="Enter your message here"></textarea><br><br>
         <input type="submit" id="submitbtn" name="submitbtn" value="Send message">
 </form>
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST) && isset($_POST['submitbtn'])) {
-    $FullName = htmlspecialchars($_POST['full_name']);
+    $FullName = htmlspecialchars(trim($_POST['full_name']));
     $EmailAddress = htmlspecialchars($_POST['email_address']);
     $Topic = htmlspecialchars($_POST['topic']);
     $Message = htmlspecialchars($_POST['message']);
+    
+    echo "<h1>$FullName $EmailAddress $Topic $Message"; // this will output the information you submitted in the form
+
+    if (empty($FullName)) {
+        echo "Please enter a name";
+    } elseif (strlen($Fullname) <= 2) {
+        echo "Please enter a name longer than 2 letters";
+    }
 }
 
 ?>
